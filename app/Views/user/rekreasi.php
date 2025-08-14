a7271e706e0c86d4b408b713ee9b3e2e28340b9a
Farlyhaydy H.Djalil <farlyhay1@gmail.com>
2025-08-14 09:05:52 +0800

    update halaman rekreasi pada toggle

--- app/Views/user/rekreasi.php
+++ app/Views/user/rekreasi.php
@@ -138,9 +138,6 @@
         </div>
     </div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-    <script src="script.js"></script>
-
     <!-- Footer -->
     <footer class="footer">
         <div class="container">
